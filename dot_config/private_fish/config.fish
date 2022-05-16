dd198d1386e318a7aa92c99c29903a66aaecfee3
Dyson <dyson@outlook.ie>
2022-05-16 15:52:15 +1000

    Add all fish shell config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+    alias vim="nvim"
+    alias vi="nvim"
+end
