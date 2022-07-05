a582dd1fd454804db6e8b2a39e764f8caff2a60b
Dyson <dyson@outlook.ie>
2022-07-05 18:34:22 +1000

    Cleanup fish config, only add config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,24 +1,21 @@
+# Fisher related
+# A plugin manager for Fish—the friendly interactive shell.
+# * NVM support, make sure you have NVM installed first.
+# > fisher install FabioAntunes/fish-nvm edc/bass
+# * z tracks the directories you visit
+# > fisher install jethrokuan/z
+#
+# Macports support
+# > fish_add_path /opt/local/bin
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-
-    alias vim="nvim"
-    alias vi="nvim"
-
     # nvm config
     function nvm
         bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
     end
     set -x NVM_DIR ~/.nvm
     nvm use default --silent
+
+    alias vim="nvim"
 end
 
-# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
-# only run in interactive (not automated SSH for example)
-#if status is-interactive
-# don't nest inside another tmux
-#and not set -q TMUX
-  # Adapted from https://unix.stackexchange.com/a/176885/347104
-  # Create session 'main' or attach to 'main' if already exists.
-  #tmux new-session -A -s main
-#end
