72c7f427f80d855fcbd5345b5c0b3ef70442048e
Dyson Lee <dyson@outlook.ie>
2022-10-03 17:20:03 +1100

    iTerm2, Kitty and Neovim updates

--- dot_config/nvim/lua/plugin-config/bufferline.lua
+++ dot_config/nvim/lua/plugin-config/bufferline.lua
@@ -1,12 +1,6 @@
 vim.opt.termguicolors = true
 require("bufferline").setup {
   options = {
-    indicator_icon = '▎',
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
     -- 使用 nvim 内置lsp
     diagnostics = "nvim_lsp",
     -- 左侧让出 nvim-tree 的位置
