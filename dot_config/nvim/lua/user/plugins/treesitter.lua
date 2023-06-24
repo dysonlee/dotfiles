b56111ce48db59b468a13ec8985bb7792e594036
Dyson <dyson@outlook.ie>
2023-06-24 15:30:49 +1000

    feat(nvim): add vue support

--- dot_config/nvim/lua/user/plugins/treesitter.lua
+++ dot_config/nvim/lua/user/plugins/treesitter.lua
@@ -2,6 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
         ensure_installed = {"markdown", "markdown_inline", "tsx", "toml", "fish", "php", "json", "yaml", "swift",
-                            "javascript", "typescript", "css", "html", "lua"}
+                            "javascript", "typescript", "css", "html", "lua", "vue"}
     }
 }
