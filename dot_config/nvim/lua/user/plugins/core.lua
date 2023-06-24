4464d0fb0acffc3e9445059c0c39230f11b210a4
Dyson <dyson@outlook.ie>
2023-06-24 15:31:17 +1000

    feat(nvim): add colorschema - neosolarized

--- dot_config/nvim/lua/user/plugins/core.lua
+++ dot_config/nvim/lua/user/plugins/core.lua
@@ -1,10 +1,10 @@
 return {{
     "goolord/alpha-nvim",
     enabled = false
-}, {{
+}, {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
         opts.winbar = nil
         return opts
     end
-}}, {"Mofiqul/vscode.nvim"}, {"windwp/nvim-ts-autotag"}}
+}, {"Mofiqul/vscode.nvim"}, {"windwp/nvim-ts-autotag"}, {"overcache/NeoSolarized"}}
