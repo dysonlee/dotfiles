82f9e921a6b20eb86544d26d4c99755d1d9e7bd4
Dyson <dyson@outlook.ie>
2023-09-15 10:23:07 +1000

    feat(nvim): incremental selection

--- dot_config/nvim/lua/user/plugins/treesitter.lua
+++ dot_config/nvim/lua/user/plugins/treesitter.lua
@@ -2,6 +2,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
         ensure_installed = {"markdown", "markdown_inline", "tsx", "toml", "fish", "php", "json", "yaml", "swift",
-                            "javascript", "typescript", "css", "html", "lua", "vue", "c_sharp"}
+                            "javascript", "typescript", "css", "html", "lua", "vue", "c_sharp"},
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<CR>",
+                node_incremental = "<CR>",
+                node_decremental = "<BS>",
+                scope_incremental = "<TAB>"
+            }
+        }
     }
 }
