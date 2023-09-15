518f504f6ddd61befb1b16f998e3734320389ad4
Dyson <dyson@outlook.ie>
2023-09-15 10:24:12 +1000

    chore: use underline cursor

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -7,7 +7,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "auto", -- sets vim.opt.signcolumn to auto
         wrap = false, -- sets vim.opt.wrap'
-        guifont = "Consolas:h25",
+        guifont = "Monaco:h23",
         linespace = 12,
         foldmethod = "manual"
     },
@@ -19,7 +19,11 @@ return {
         diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
         icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
         ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+        -- **************** neovide settings ****************
         neovide_transparency = 0.0,
+        linespace = 2,
         neovide_background_color = "#1e1e1e" .. string.format("%x", math.floor(255 * 0.95)),
+        neovide_cursor_animate_in_insert_mode = false
+        -- **************************************************
     }
 }
