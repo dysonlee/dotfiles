46da77d62f613adaa8dce4ebe45093bf6c03b7b4
Dyson Lee <dyson@outlook.ie>
2024-12-14 14:05:51 +1000

    fix: fix neovim invalid commands

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -15,10 +15,6 @@ return {
     opts = {
       options = {
         diagnostics = "nvim_lsp",
-        -- stylua: ignore
-        close_command = function(n) require("mini.bufremove").delete(n, false) end,
-        -- stylua: ignore
-        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         always_show_bufferline = true,
       },
     },
